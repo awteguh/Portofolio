100684fb65663fd7defa091fd33a10929a3f4f7b
awteguh <awteguh@gmail.com>
2026-07-21 20:01:05 +0700

    update

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -2,6 +2,20 @@ import type { Certification } from "./types"
 
 export const certifications: Certification[] = [
   {
+    name: "Introduction to Cybersecurity",
+    issuer: "Cisco Networking Academy",
+    year: "2026",
+    image: "/images/certs/intro-cybersecurity.jpg",
+    pdfUrl: "/images/Introduction_to_Cybersecurity.pdf",
+  },
+  {
+    name: "NDG Linux Essentials",
+    issuer: "Cisco Networking Academy",
+    year: "2023",
+    image: "/images/certs/linux-essentials.jpg",
+    pdfUrl: "/images/Partner-_NDG_Linux_Essentials.pdf",
+  },
+  {
     name: "Fundamental Cyber Security",
     issuer: "Digitalent Kominfo",
     year: "2024",
@@ -35,5 +49,12 @@ export const certifications: Certification[] = [
     year: "2024",
     image: "/images/certs/udemy-cyber-sec.jpg",
     pdfUrl: "/images/udemy cyber sec.pdf",
+  },
+  {
+    name: "Networking Basics",
+    issuer: "Cisco Networking Academy",
+    year: "2026",
+    image: "/images/certs/networking-basic.jpg",
+    pdfUrl: "/images/Networking_Basic.pdf",
   },
 ]
